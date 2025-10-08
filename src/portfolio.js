7dc2945e710652ad98d2e1a029ecd7cd01cf1976
cldelacruz-dev <carllorenze16delacruz@gmail.com>
2025-10-08 15:31:16 +0800

    feat (fe): Update CV path

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native / Laravel and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1VA_qHxW8JgntnTHEegJMWikBmsG0QYSw/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1UZxOFk7GYkhTLZUqaKfqjNzA3a6h72cl/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
